dffb2486ba7ef58129e6f3e04ecf6818d0ffb010
Marcus Stevenson <mdstevenson99@gmail.com>
2019-07-29 13:41:44 -0600

    added start btn, render random responses in random order functionality

--- index.js
+++ index.js
@@ -10,15 +10,58 @@ const questionAns = [
     {painting: "Ram's Head", artist: "Georgia O'Keefe", pImg: "", aImg: ""},
     {painting: "Woman with a Pearl Neckalce", artist: "Mary Cassat", pImg: "", aImg: ""},
 ]
+let questionCounter= 0;
+let scoreCounter= 0;
+
 function startQuiz(){
     //render start page
     //listen for click on start button and render first queston screen
     // listen for click on restart button and render first question, reset score
+    $('.js-restartBtn').on('click', function(){
+        alert('this');
+    });
+    $('.js-startBtn').on('click', function(e){
+        $('.js-startPg').remove();
+        renderQuestion();
+    });
+}
+function arrayShuffler(currentQNum, inArray){
+    let ansArray = inArray;
+    ansArray.splice(currentQNum, 1);
+    let currentIndex = ansArray.length;
+    let temporaryValue;
+    let randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+       randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = ansArray[currentIndex];
+        ansArray[currentIndex] = ansArray[randomIndex];
+        ansArray[randomIndex] = temporaryValue;
+    }
+    return ansArray;
 }
 function renderQuestion(){
     //display painting in prompt element
     //pick 3 random artists from the other paintings
     //populate answer buttons with resources/info
+    let currentPainting = questionAns[questionCounter].painting;
+    let currentCorrect = questionAns[questionCounter].artist;
+    let currentQ = questionCounter;
+    let falseAnsArray = arrayShuffler(currentQ, questionAns);
+    questionCounter +=1;
+    let scrambleAns = [falseAnsArray[0].artist, falseAnsArray[1].artist, falseAnsArray[4].artist, falseAnsArray[2].artist, currentCorrect];
+    arrayShuffler(1, scrambleAns);
+    alert(currentCorrect);
+    $('.js-Q-Counter').html(`Question: ${questionCounter}/10`);
+    $('.js-Q-box').html(`<h2>Who painted ${currentPainting}?</h2>`);
+    $('.js-A-box').html(
+        `<button type="button" class="responseBtn">${scrambleAns[0]}</button>`+
+        `<button type="button" class="responseBtn">${scrambleAns[1]}</button>`+
+        `<button type="button" class="responseBtn">${scrambleAns[2]}</button>`+ 
+        `<button type="button" class="responseBtn">${scrambleAns[3]}</button>`);
 }
 function onAnsClick(){
     //listen for click on answer buttons
